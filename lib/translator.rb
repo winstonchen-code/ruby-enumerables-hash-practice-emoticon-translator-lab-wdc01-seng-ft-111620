05e157fbdab5597c363fff9aae88f407abb338e1
Winston Chen <winstonchen.wm@gmail.com>
2020-11-09 03:38:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(path)
     emoji_hash["get_meaning"][emojis[1]] = word
     emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
   end
-  emoji_hash
+  puts emoji_hash
 end
   # code goes here
 
