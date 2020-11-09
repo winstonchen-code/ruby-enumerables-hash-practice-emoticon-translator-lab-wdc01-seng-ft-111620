7605f5d9ba5b97fd6dd0b30548bb567ae26316f2
Winston Chen <winstonchen.wm@gmail.com>
2020-11-09 04:16:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,6 +10,7 @@ def load_library(path)
     final_hash[key][:english] = value[0]
     final_hash[key][:japanese] = value[1]
   end
+  final_hash
   # code goes here
 
 def get_japanese_emoticon(path, emoticon)
