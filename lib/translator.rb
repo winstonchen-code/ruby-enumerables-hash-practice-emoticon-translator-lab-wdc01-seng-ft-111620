7958d572057142c7a0eefe0ccd3b94467db001d2
Winston Chen <winstonchen.wm@gmail.com>
2020-11-09 04:20:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(path, emoticon)
       return value[:japanese]
     end
   end
-  return "Sorry, that moticon was not found"
+  return "Sorry, that emoticon was not found"
 end
   # code goes here
 
